b5dc23c663914ef99193a7746539795782743dca
Mark-A-A <markawai01@gmail.com>
2015-12-08 17:43:14 -0500

    added new Projects page and started JS sheet, AJAX from my GitHub

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  $("#about").on(click, function(){
-    alert("it works!");
-  });
+  alert("it works!");
+  $.ajax({
+    type: "GET",
+    url: "https://api.github.com/users/Mark-A-A/repos",
+    success: function(repos){
+      console.log("Holy Cow! Mark's GitHub API Endpoint Connection");
+    }
+  })
+
+  //$("#about").on(click, function(){
+    
+  //});
 });
