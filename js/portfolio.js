9a1a23c187a5ed100616e215009508410c55f227
Mark-A-A <markawai01@gmail.com>
2015-12-08 18:34:32 -0500

    added Repos through JS, need commit linksgit add .

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,8 +5,31 @@ $(document).ready(function(){
     url: "https://api.github.com/users/Mark-A-A/repos",
     success: function(repos){
       console.log("Holy Cow! Mark's GitHub API Endpoint Connection");
-    }
+      //debugger
+      for(var i=0; i < repos.length; i++) {
+        var newRepoURL = buildRepoList(repos[i]);
+        console.log(newRepoURL);
+        $(".list-group").append("<p>"+newRepoURL+"</p>"); //Appended JSON to the page
+      }
+    },
+    error: function (jqXHR, textStatus, errorThrown){
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    },
   })
+
+  function buildRepoList(repoData){
+    var commitsAPIUrl = "https://api.github.com/repos"; //API URL
+    commitsAPIUrl += repoData.owner.login +"/";       //looking at object info and adding it
+    commitsAPIUrl += repoData.name + "/commits";
+
+    var newLink = $("<a>")
+      .attr("href", commitsAPIUrl)
+      .addClass("list-group-item")
+      .append(repoData.full_name);
+    return newLink;
+  };
 
   //$("#about").on(click, function(){
     
