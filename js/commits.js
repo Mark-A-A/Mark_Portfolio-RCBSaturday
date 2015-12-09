21db1a14d6e400a1b4dcab60a083d2b88aeb3a07
Mark-A-A <markawai01@gmail.com>
2015-12-09 03:04:24 -0500

    AJAX commits - success; next up is error handling

--- js/commits.js
+++ js/commits.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   
   $(document).on("click", "a", function(e){
-    e.preventDefault();
+    e.preventDefault(); //prevent the user from going to JSON commits page
     alert("Hey Committer - you clicked a link");
+    
+     $.ajax({  //Adding ajax request based on the link of repo
+       type: "GET",
+       url: $(this).attr("href"), //Event listener for links of repos
+       success: function (commits) {
+         console.log(commits);
+       }
+     });
+    
   });
 });
