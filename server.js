a9f793708f8e8bd9f229700376a48512c0873cfe
Mark-A-A <markawai01@gmail.com>
2016-02-09 16:22:08 -0500

    Initial server set up. Adding handling for page requests next.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 var express = require("express");
+var app = express();
+var PORT = 8080;
+
+ 
+app.get('/', function (req, res) {
+  console.log("On the homepage..Hello client user");
+  res.send('Hello World');
+})
+ 
+app.listen(PORT);
