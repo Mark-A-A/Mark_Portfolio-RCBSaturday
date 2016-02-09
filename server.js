10a27ad9c3c3042857c902aba8182ffb708a6396
Mark-A-A <markawai01@gmail.com>
2016-02-09 17:56:22 -0500

    added start in package.json for deployment. Added get request response handlers for home page, working on blog and portfolio.  Now need middleware

--- server.js
+++ server.js
@@ -3,9 +3,31 @@ var app = express();
 var PORT = 8080;
 
  
-app.get('/', function (req, res) {
-  console.log("On the homepage..Hello client user");
-  res.send('Hello World');
+app.use('/js', express.static('public/js'));    //static routes
+app.use('/css', express.static('public/css'));
+
+app.get('/', function (req, res) {                    //Home Page response
+  console.log("Hello client user..On the homepage");
+  console.log(`Current directory: ${process.cwd()}`);
+  //res.send('Hello World');
+  res.sendFile(process.cwd() + '/views/home.html');
 })
+
+app.get('/blog', function (req, res) {
+  console.log("On the blog page.");
+  console.log(`Current directory: ${process.cwd()}`);
+  //res.send('Hello World');
+  res.sendFile(process.cwd() + '/views/home.html');
+});
+
+app.get('/portfolio', function (req, res) {
+  console.log("On the Portfolio page.");
+  console.log(`Current directory: ${process.cwd()}`);
+  //res.send('Hello World');
+  res.sendFile(process.cwd() + '/views/home.html');
+});
+
  
-app.listen(PORT);
+app.listen(PORT, function(){
+  console.log("Listening on Port %s", PORT);
+});
